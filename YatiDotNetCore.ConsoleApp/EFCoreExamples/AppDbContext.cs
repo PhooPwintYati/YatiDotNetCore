ec2b1038acaf2d9330f47108e6093994097c9593
yati <yati18660@gmail.com>
2023-12-27 00:42:39 +0630

    Add project files.

---
+++ YatiDotNetCore.ConsoleApp/EFCoreExamples/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using YatiDotNetCore.ConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YatiDotNetCore.ConsoleApp.EFCoreExamples
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = "TITI\\MSSQLSERVER2019",
+                InitialCatalog = "YatiDotNetCore",
+                UserID = "sa",
+                Password = "022640",
+                TrustServerCertificate = true
+            };
+            optionsBuilder.UseSqlServer(_sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
